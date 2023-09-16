f969d956810c30a517b3cb1081c874db7e984284
Santiago Franco <ktns0930@gmail.com>
2023-09-15 20:55:46 -0500

    Extracted custom hook from App to useFilters hook

--- shopping-car-reducer-usecontext/src/App.jsx
+++ shopping-car-reducer-usecontext/src/App.jsx
@@ -1,29 +1,12 @@
 import { products as initialProducts } from "./mocks/products.json";
 import { Products } from "./components/Products";
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { IS_DEVELOPMENT } from "./config";
-import { FiltersContext } from "./Context/filters";
+import { useFilters } from "./Hooks/useFilters";
 
 // Custom hook
-function useFilters() {
-  /*const [filters, setFilters] = useState({
-    category: "all",
-    minPrice: 0,
-  });*/
-
-  const {filters, setFilters} = useContext(FiltersContext)
-  const filterProducts = (products) => {
-    return products.filter((product) => {
-      return (
-        product.price >= filters.minPrice &&
-        (filters.category === "all" || filters.category === product.category)
-      );
-    });
-  };
-  return { filters, filterProducts, setFilters };
-}
 
 function App() {
   const [products] = useState(initialProducts);
